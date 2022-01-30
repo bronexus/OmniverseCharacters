43754bea71d60952f9262ede1bbff2435b60f5e0
mononixaware <mononixaware@gmail.com>
2022-01-30 16:12:55 +0200

    Initial Commit

---
+++ OmniverseCharacters/OmniverseCharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmniverseCharactersApp.swift
+//  OmniverseCharacters
+//
+//  Created by Dumitru on 30.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct OmniverseCharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
